2d45930e6bade308af2db936cfb55305853ac3fb
Csaba Balazs <csaba.balazs@evozon.com>
2013-10-30 15:58:11 +0200

    Added message to Web context, removed unnecessary Math adapter method, removed error reporting from index.

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
-
+/**
+ * Autoload with Composer
+ */
 require_once __DIR__.'/vendor/autoload.php';
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(-1);
-
-date_default_timezone_set('UTC');
-set_time_limit(0);
 
 use Gorkana\Calculator as Calculator;
 
+/**
+ * Run context aware calculator application
+ */
 $application = new Calculator\App();
 $application->run();
